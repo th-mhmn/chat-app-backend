db2a9d8aa915199daeda36c04ea35a0ef14c03e5
Taha <tahamehmandoust@gmail.com>
2025-11-21 11:38:24 +0330

    feat(user): set up user schema

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  username: string;
+  @Prop()
+  name: string;
+  @Prop()
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
