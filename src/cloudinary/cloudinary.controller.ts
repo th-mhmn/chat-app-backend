a267f7afec9e09c01a483b6291ab64c7db7a7ea2
Taha <tahamehmandoust@gmail.com>
2025-11-24 19:40:22 +0330

    feat(upload): implement multiple file uploads

--- src/cloudinary/cloudinary.controller.ts
+++ src/cloudinary/cloudinary.controller.ts
@@ -2,9 +2,10 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { CloudinaryService } from './cloudinary.service';
 
 @Controller('image')
@@ -13,7 +14,39 @@ export class CloudinaryController {
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  uploadImage(@UploadedFile() file: Express.Multer.File) {
-    return this.cloudinaryService.uploadFile(file);
+  async uploadImage(@UploadedFile() file: Express.Multer.File) {
+    const result = await this.cloudinaryService.uploadFile(file);
+    const { secure_url, version, display_name, format, resource_type } = result;
+    return {
+      message: 'Upload Successful',
+      data: {
+        url: secure_url,
+        version,
+        display_name,
+        format,
+        resource_type,
+      },
+    };
+  }
+
+  @Post('upload-multiple')
+  @UseInterceptors(FilesInterceptor('files', 5))
+  async uploadImages(@UploadedFiles() files: Express.Multer.File[]) {
+    const result = await this.cloudinaryService.uploadMultipleFiles(files);
+    return {
+      message: 'Upload Successful',
+      data: result.map((res) => {
+        const { secure_url, version, display_name, format, resource_type } =
+          res;
+
+        return {
+          url: secure_url,
+          version,
+          display_name,
+          format,
+          resource_type,
+        };
+      }),
+    };
   }
 }
