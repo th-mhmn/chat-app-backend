50c0c45640e8167a699e14f411092a151f7b5c07
Taha <tahamehmandoust@gmail.com>
2025-11-24 14:48:43 +0330

    feat(post): add author props to post response dto

--- src/post/dto/response-post.dto.ts
+++ src/post/dto/response-post.dto.ts
@@ -1,18 +1,37 @@
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
+import { PostDocument } from '../schemas/post.schema';
 
 export class ResponsePostDto {
   @Expose()
   _id: string;
+
   @Expose()
   backgroundColor: string;
+
   @Expose()
   content: string;
+
   @Expose()
   mediaUrls: string[];
+
   @Expose()
   visibility: IVisibility;
+
+  @Expose()
+  @Transform(({ obj }: { obj: PostDocument }) => obj.author._id)
+  authorId: string;
+
+  @Expose()
+  @Transform(({ obj }: { obj: PostDocument }) => obj.author.name)
+  authorName: string;
+
+  @Expose()
+  @Transform(({ obj }: { obj: PostDocument }) => obj.author.email)
+  authorEmail: string;
+
   @Expose()
   createdAt: Date;
+
   @Expose()
   updatedAt: Date;
 }
