a548380cc2b102eb6ac720b7ba4988d91303dfb2
Taha <tahamehmandoust@gmail.com>
2025-11-28 21:09:48 +0330

    feat(user): expand user update dto

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,25 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import {
+  IsDateString,
+  IsOptional,
+  IsPhoneNumber,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @MaxLength(128)
+  @IsOptional()
+  bio?: string;
+  @MinLength(4)
+  @MaxLength(32)
+  @IsOptional()
+  username?: string;
+  @IsOptional()
+  name?: string;
+  @IsOptional()
+  @IsDateString()
+  birthDate?: Date;
+  @IsPhoneNumber()
+  @IsOptional()
+  phoneNumber?: string;
+}
