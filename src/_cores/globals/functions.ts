e11ceefb30a7862368c3ff1637c58555944ed161
Taha <tahamehmandoust@gmail.com>
2025-12-01 18:03:21 +0330

    fix(transform-url): add pre-check to obj

--- src/_cores/globals/functions.ts
+++ src/_cores/globals/functions.ts
@@ -1,2 +1,2 @@
 export const transformMediaUrl = (obj) =>
-  `https://res.cloudinary.com/${process.env.CLOUDINARY_NAME}/${obj.resource_type}/upload/v${obj.version}/${obj.display_name}`;
+  `https://res.cloudinary.com/${process.env.CLOUDINARY_NAME}/${obj?.resource_type}/upload/v${obj?.version}/${obj?.display_name}`;
