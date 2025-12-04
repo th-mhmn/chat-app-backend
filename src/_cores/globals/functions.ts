0ab6ce35a285143efbd058b24fc79b23c7ba8e0f
Taha <tahamehmandoust@gmail.com>
2025-12-04 21:44:26 +0330

    fix(transformMediaUrl): return null if object is undefined

--- src/_cores/globals/functions.ts
+++ src/_cores/globals/functions.ts
@@ -1,2 +1,4 @@
 export const transformMediaUrl = (obj) =>
-  `https://res.cloudinary.com/${process.env.CLOUDINARY_NAME}/${obj?.resource_type}/upload/v${obj?.version}/${obj?.display_name}`;
+  obj
+    ? `https://res.cloudinary.com/${process.env.CLOUDINARY_NAME}/${obj.resource_type}/upload/v${obj.version}/${obj.display_name}`
+    : null;
