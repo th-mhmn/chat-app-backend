9eede35221033f7e1946c40c8761157bf2c394c7
Taha <tahamehmandoust@gmail.com>
2025-12-05 18:52:01 +0330

    feat(message): add update message dto

--- src/message/dto/update-message.dto.ts
+++ src/message/dto/update-message.dto.ts
@@ -1,1 +1,10 @@
-export class UpdateMessageDto {}
+import { IsArray, IsOptional } from 'class-validator';
+import { MediaType } from 'src/_cores/globals/classes';
+
+export class UpdateMessageDto {
+  @IsOptional()
+  text: string;
+  @IsOptional()
+  @IsArray()
+  mediaFiles: MediaType[];
+}
