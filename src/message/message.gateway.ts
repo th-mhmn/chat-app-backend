84ffa1282c56888c4d9b1e88b193c62d34e8e324
Taha <tahamehmandoust@gmail.com>
2025-12-07 20:25:10 +0330

    feat(message): implement gateway for adding and editing messages

--- src/message/message.gateway.ts
+++ src/message/message.gateway.ts
@@ -1,9 +1,50 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { ResponseMessageDto } from './dto/response-message.dto';
+@WebSocketGateway({ cors: { origin: '*' } })
+export class MessageGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
 
-@WebSocketGateway()
-export class MessageGateway {
+  handleConnection(client: Socket) {
+    console.log(`Client connect ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnect ${client.id}`);
+  }
+
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  handleEvent(
+    @MessageBody() conversationId: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    client.join(conversationId);
+  }
+
+  @SubscribeMessage('join_conversation')
+  handleJoinConversation(
+    @MessageBody() data: string,
+    @ConnectedSocket() client: Socket,
+  ): string {
+    return data;
+  }
+
+  handleNewMessage(conversationId: string, data: ResponseMessageDto) {
+    this.server.to(conversationId).emit('new_message', data);
+  }
+
+  handleUpdateMessage(conversationId: string, data: ResponseMessageDto) {
+    this.server.to(conversationId).emit('update_message', data);
   }
 }
